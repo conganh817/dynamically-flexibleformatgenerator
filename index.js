178c9e41a0e31f017754714db5577c7686dfcbac
conganh817 <simoneto06nam1311@outlook.com>
2024-05-07 16:01:37 +0700

    First commit

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-function groupAnagrams(strs) {
-  const map = new Map();
-  for (const str of strs) {
-    const sortedStr = str.split("").sort().join("");
-    if (!map.has(sortedStr)) map.set(sortedStr, []);
-    map.get(sortedStr).push(str);
+function generate(numRows) {
+  const triangle = [];
+  for (let i = 0; i < numRows; i++) {
+    const row = new Array(i + 1).fill(1);
+    for (let j = 1; j < row.length - 1; j++) {
+      row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+    }
+    triangle.push(row);
   }
-  return [...map.values()];
+  return triangle;
 }
