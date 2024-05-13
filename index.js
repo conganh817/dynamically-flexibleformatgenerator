d30511f6227e624e9a7f8dad42d6d28a8ef20e1c
conganh817 <simoneto06nam1311@outlook.com>
2024-05-13 07:37:42 +0700

    demo task

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const number = 10;
-if (number % 2 === 0) {
-  console.log("Even number");
-} else {
-  console.log("Odd number");
+function sortedArrayToBST(nums) {
+  if (!nums.length) return null;
+  const mid = Math.floor(nums.length / 2);
+  const root = new TreeNode(nums[mid]);
+  root.left = sortedArrayToBST(nums.slice(0, mid));
+  root.right = sortedArrayToBST(nums.slice(mid + 1));
+  return root;
 }
